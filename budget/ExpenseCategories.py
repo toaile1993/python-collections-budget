c607ab71bfce3f85c90d4e1986efa1e938c97b12
Toai <toaile@localhost.localdomain>
2021-01-01 12:21:35 -0500

    Add timeit

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,5 +1,6 @@
 from . import Expense
 import matplotlib.pyplot as plt
+import timeit
 
 def main():
     expenses = Expense.Expenses()
@@ -14,7 +15,27 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
                 print("Sets are NOT equal by subset test")
 
+    print(timeit.timeit(stmt='expenses.categorize_for_loop()',
+    setup = 
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+    number = 100000,
+    globals = globals()
+    ))
 
+    print(timeit.timeit(stmt='expenses.categorize_set_comprehension()',
+    setup = 
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+    number = 100000,
+    globals = globals()
+    ))
 
 if __name__ == "__main__":
     main()
