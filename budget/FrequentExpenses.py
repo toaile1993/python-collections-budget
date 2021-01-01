92a85c0c3908bb3f7837215778774b3c6805085d
Toai <toaile@localhost.localdomain>
2020-12-31 19:41:16 -0500

    Task 3

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,3 +1,14 @@
 from . import Expense
+
+#Task 2
 expenses = Expense.Expenses()
-expenses.read_expenses()
+expenses.read_expenses(data/spending_data.csv)
+
+#Task 3
+spending_categories = []
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+
+
