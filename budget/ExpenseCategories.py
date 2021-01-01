771b45217af93b82ce88f60f387ca4accf52efc7
Toai <toaile@localhost.localdomain>
2021-01-01 12:32:20 -0500

    Complete Module 5

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -36,6 +36,20 @@ expenses.read_expenses('data/spending_data.csv')
     number = 100000,
     globals = globals()
     ))
+    
+    fig,ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
+
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
+
+    ax.pie(divided_expenses_sum, labels = labels, autopct = '%1.1f%%')
+
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
     main()
