5e5ba4c359cdb47cdbb71c474b13ee0c55f7a9db
Toai <toaile@localhost.localdomain>
2020-12-31 20:35:30 -0500

    Task 4

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,4 +1,5 @@
 from . import Expense
+import collections
 
 #Task 2
 expenses = Expense.Expenses()
@@ -10,5 +11,7 @@ spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
-
+#Task 4
+spending_counter = collections.Counter(spending_categories) 
+print(spending_counter)
 
